03de40190db6d05cedc7668ffabf7476db64787f
roshane <roshanemadusanka@gmail.com>
2018-06-02 23:15:25 +0530

    Initial commit

---
+++ src/main/java/com/aeon/hkrnk/str/StrongPassword.java
@@ -0,0 +1,17 @@
+package com.aeon.hkrnk.str;
+
+/**
+ * Created by roshane on 6/2/2018.
+ */
+public class StrongPassword {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Complete the minimumNumber function below.
+    static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+
+    }
+}
