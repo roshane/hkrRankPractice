b6a7bca5344b2e199e03b83bd8e65642a66b3417
roshane <roshanemadusanka@gmail.com>
2018-06-03 06:20:08 +0530

    - Day #2

---
+++ src/main/java/com/aeon/hkrnk/str/TwoCharacters.java
@@ -0,0 +1,17 @@
+package com.aeon.hkrnk.str;
+
+/**
+ * Created by roshane on 6/3/2018.
+ */
+public class TwoCharacters {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Complete the twoCharaters function below.
+    static int twoCharaters(String s) {
+
+
+    }
+}
